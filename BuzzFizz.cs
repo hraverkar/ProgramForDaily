051b2187cb9d26ad7b143bb4ce8172dabd7ba7cb
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-05-19 20:12:29 +0530

    Create BuzzFizz.cs

---
+++ BuzzFizz.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace FizzBuzz
+{
+  internal static class Program
+  {
+    public static void Main(string[] args)
+    {
+      for (var i = 1; i <= 100; i++)
+      {
+        if ((i % 3 == 0) && (i % 5 == 0))
+          Console.WriteLine("FizzBuzz");
+        else if (i % 3 == 0)
+          Console.WriteLine("Fizz");
+        else if (i % 5 == 0)
+          Console.WriteLine("Buzz");
+        else
+          Console.WriteLine(i);
+      }
+    }
+  }
+}
