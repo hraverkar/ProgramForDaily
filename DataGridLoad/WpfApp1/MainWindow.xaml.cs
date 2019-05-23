185e8ae87832ccc1deaf2f67d505a4bf78ff5d20
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-05-23 23:14:48 +0530

    DataGrid_Loaded Added in Project.

---
+++ DataGridLoad/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      string ConnectionString = @"Data Source = (localdb)\ProjectsV13; Initial Catalog = TestDatabase; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+      SqlConnection conn = new SqlConnection(ConnectionString);
+      string sql = "Select * from Customers";
+      conn.Open();
+      SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, conn);
+      DataTable dataSet = new DataTable("Customers");
+      sqlDataAdapter.Fill(dataSet);
+      Grid1.ItemsSource = dataSet.DefaultView;
+      conn.Close();
+    }
+    class Human
+    {
+      public string Fname { get; set; }
+      public string Lname { get; set; }
+
+      public Human(string fname, string lname)
+      {
+        this.Fname = fname;
+        this.Lname = lname;
+      }
+  }
+
+    private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+    {
+      var list = new List<Human>
+      {
+        new Human("Harshal", "Raverkar"),
+        new Human("Shefali", "Sen"),
+        new Human("Vikas", "Sharma"),
+        new Human("Vijay", "Raj"),
+        new Human("Virat", "Kohli")
+      };
+      var grid = sender as DataGrid;
+      grid.ItemsSource = list;
+    }
+  }
+}
