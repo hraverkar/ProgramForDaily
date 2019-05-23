9de613debcd9543aadb4da1ac3ba6746d9d5e68d
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-05-23 23:51:09 +0530

    ReadData from text file and show in WPF Application.

---
+++ ReadDataFromText/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+    List<Patient> _list;
+    public void DataGrid_Loaded(object sender,RoutedEventArgs e)
+    {
+      var patients = new List<Patient>();
+      using (StreamReader reader = new StreamReader(@"C:\Temp\Data.txt"))
+      {
+        while(true)
+        {
+          string line = reader.ReadLine();
+          if(line == null)
+          { break; }
+          patients.Add(new Patient(line));
+        }
+      }
+      this._list = patients;
+      var grid = sender as DataGrid;
+      grid.ItemsSource = patients;
+    }
+
+
+
+    private void Window_Closing(object sender, CancelEventArgs e)
+    {
+      using(StreamWriter writer = new StreamWriter(@"C:\Temp\Data.txt"))
+      {
+        foreach(Patient patient in this._list)
+        {
+          writer.WriteLine(patient.GetLine());
+        }
+      }
+    }
+  }
+}
---
+++ ReadDataFromText/WpfApp1/Patient.cs
@@ -0,0 +1,24 @@
+﻿namespace WpfApp1
+{
+  internal class Patient
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Code { get; set; }
+    public int Insurance { get; set; }
+
+    public Patient(string line)
+    {
+      string[] parts = line.Split(',');
+      this.FirstName = parts[0];
+      this.LastName = parts[1];
+      this.Code = parts[2];
+      this.Insurance = int.Parse(parts[3]);
+    }
+
+    public string GetLine()
+    {
+      return this.FirstName + "," + this.LastName + "," + this.Code + "," + this.Insurance.ToString();
+    }
+  }
+}
