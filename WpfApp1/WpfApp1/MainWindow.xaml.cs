9b52ee4db5e2cd94e9ccfe4dca17b772f085dab8
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-05-23 22:31:50 +0530

    ADO.NET Code Example Added.

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      string ConnectionString = @"Data Source = (localdb)\ProjectsV13; Initial Catalog = TestDatabase; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+      SqlConnection conn = new SqlConnection(ConnectionString);
+      string sql = "Select * from Customers";
+      conn.Open();
+      SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sql, conn);
+      DataTable dataSet = new DataTable("Customers");
+      sqlDataAdapter.Fill(dataSet);
+      Grid1.ItemsSource = dataSet.DefaultView;
+      conn.Close();
+    }
+  }
+}
