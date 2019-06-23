ee4d3acfaad0411ec36d6df9343c28db2b5d149d
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-06-23 19:18:27 +0530

    Update Is_Unique.cs

--- Is_Unique.cs
+++ Is_Unique.cs
@@ -7,8 +7,10 @@ namespace ConsoleApp19
     public static void Main()
     {
       string str = "abcd10jk";
+      string str1 = "hello Harshl";
       var t = Is_UniqueKeyWord(str);
-      if(t == true)
+      var t1 = Is_UniqueKeyWord1(str1);
+      if (t == true)
       {
         Console.WriteLine("This string is has unique keyword");
       }
@@ -16,6 +18,30 @@ namespace ConsoleApp19
       {
         Console.WriteLine("This string is has no unique keyword");
       }
+
+      if (t1 == true)
+      {
+        Console.WriteLine("This string is has unique keyword");
+      }
+      else
+      {
+        Console.WriteLine("This string is has no unique keyword");
+      }
+    }
+
+    private static bool Is_UniqueKeyWord1(string str)
+    {
+      char[] ch = str.ToCharArray();
+      Array.Sort(ch);
+
+      for(int i=0;i<ch.Length-1;i++)
+      {
+        if (ch[i] != ch[i + 1])
+          continue;
+        else
+          return false;
+      }
+      return true;
     }
 
     private static bool Is_UniqueKeyWord(string str)
