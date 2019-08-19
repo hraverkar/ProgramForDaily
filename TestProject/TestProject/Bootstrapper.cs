05fd1a024df4f8d432053ab61114af6f6f7b1bd8
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-08-19 22:47:22 +0530

    Using of Cliburn Micro Application Example ..

---
+++ TestProject/TestProject/Bootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using TestProject.ViewModels;
+
+namespace TestProject
+{
+  public class Bootstrapper:BootstrapperBase
+  {
+    private SimpleContainer _container;
+
+    public Bootstrapper()
+    {
+      Initialize();
+    }
+
+    protected override void Configure()
+    {
+      _container = new SimpleContainer();
+      _container.Singleton<IWindowManager, WindowManager>();
+      _container.PerRequest<ShellViewModel>();
+    }
+
+    protected override void OnStartup(object sender, StartupEventArgs e)
+    {
+      DisplayRootViewFor<ShellViewModel>();
+    }
+
+    protected override object GetInstance(Type service, string key)
+    {
+      return _container.GetInstance(service, key);
+    }
+
+    protected override IEnumerable<object> GetAllInstances(Type service)
+    {
+      return _container.GetAllInstances(service);
+    }
+
+    protected override void BuildUp(object instance)
+    {
+      _container.BuildUp(instance);
+    }
+  }
+}
---
+++ TestProject/TestProject/ViewModels/ShellViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+
+namespace TestProject.ViewModels
+{
+  internal class ShellViewModel :PropertyChangedBase
+  {
+    private string _name;
+
+    public string Name
+    {
+      get => _name;
+      set
+      {
+        _name = value;
+        NotifyOfPropertyChange(()=>Name);
+        NotifyOfPropertyChange(()=>CanSayHello);
+      }
+    }
+
+    public bool CanSayHello => !string.IsNullOrWhiteSpace(Name);
+
+    public void SayHello()
+    {
+      MessageBox.Show($"Hello {Name}");
+      Name = string.Empty;
+    }
+  }
+}
